60e6a54a462c8e04944ea52edb4bb9a10ff37d5a
J Bruno <jbrunorodrigues13@gmail.com>
2026-05-12 17:37:22 -0300

    implement paginated sales report with optional filters

--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,14 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Optional;
 
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
@@ -19,5 +25,30 @@ public class SaleService {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	public Page<SaleReportDTO> searchAll(String minDate, String maxDate, String name, Pageable pageable) {
+
+		LocalDate min;
+		LocalDate max;
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+		if (maxDate == null) {
+			max = today;
+		} else {
+			max = LocalDate.parse(maxDate);
+		}
+
+		if (minDate == null) {
+			min = max.minusYears(1L);
+		} else {
+			min = LocalDate.parse(minDate);
+		}
+		
+		if (name == null) {
+			name = "";
+		}
+
+		return repository.search(min, max, name, pageable);
 	}
 }
